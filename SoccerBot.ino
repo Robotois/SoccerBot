61fd312553d298386bafa3dc4529748a66fffffc
manduks <iam@armando.mx>
2018-10-06 02:33:20 -0500

    set motors to false

--- SoccerBot.ino
+++ SoccerBot.ino
@@ -3,14 +3,17 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 
-const char* ssid = "yovany-dev";
-const char* password = "MEcr0839";
-const char* brokerAdd = "10.42.0.1";
+const char* ssid = "Wireless-N";
+const char* password = "robotois8899";
+const char* brokerAdd = "192.168.10.102";
 const char* clientId = "SoccerBot-01";
 String driveTopic = "SoccerBots/tablet-01/soccerBot-01/drive";
+unsigned long timerr;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+
 
 uint8_t motorIdx = 0;
 uint8_t pwmPin = 2;
@@ -20,41 +23,31 @@ uint8_t cwPin = 16;
 uint8_t ccwPin = 17;
 
 void setupWifi() {
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to: ");
-  Serial.println(ssid);
 
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
   }
 
   randomSeed(micros());
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
 }
 
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
+    //Serial.print("Attempting MQTT connection...");
     // Attempt to connect
     if (client.connect(clientId)) {
-      Serial.println("connected");
+      //Serial.println("connected");
       // Once connected, publish an announcement...
       // client.publish("outTopic", "hello world");
       // ... and resubscribe
       client.subscribe(driveTopic.c_str());
     } else {
-      Serial.print("failed, rc=");
-      Serial.println(client.state());
-      // Serial.println(" try again in 5 seconds");
+      //Serial.print("failed, rc=");
+      //Serial.println(client.state());
+      // //Serial.println(" try again in 5 seconds");
       // Wait 5 seconds before retrying
       delay(1000);
     }
@@ -63,6 +56,10 @@ void reconnect() {
 
 void mqttLoop() {
   if (!client.connected()) {
+    setMotorPWM(0, 0);
+    setMotorPWM(0, 2);
+    setMotorPWM(0, 1);
+    setMotorPWM(0, 3);
     reconnect();
   }
   client.loop();
@@ -73,7 +70,7 @@ void drive(byte* payload) {
   JsonObject& root = jsonBuffer.parseObject(payload);
 
   if(!root.success()) {
-    Serial.println("[JSON Error] -> parseObject error");
+    //Serial.println("[JSON Error] -> parseObject error");
     return;
   }
 
@@ -84,7 +81,7 @@ void drive(byte* payload) {
   setMotorPWM(y * 1024, 1);
   setMotorPWM( - y * 1024, 3);
   float rotation = root["rotation"];
-  // Serial.println("JSON Parsed: { x: " + String(x) + ", y: " + String(y) + ", rotation: " + String(rotation) + " }");
+  // //Serial.println("JSON Parsed: { x: " + String(x) + ", y: " + String(y) + ", rotation: " + String(rotation) + " }");
 }
 
 void leds(byte* payload) {
@@ -92,20 +89,22 @@ void leds(byte* payload) {
   JsonObject& root = jsonBuffer.parseObject(payload);
 
   if(!root.success()) {
-    Serial.println("[JSON Error] -> parseObject error");
+    //Serial.println("[JSON Error] -> parseObject error");
     return;
   }
   uint8_t celebrate = root["celebrate"];
-  Serial.println("JSON Parsed: { celebrate: " + String(celebrate) + " }");
+  //Serial.println("JSON Parsed: { celebrate: " + String(celebrate) + " }");
 }
 
 void messageProcessor(char* topic, byte* payload, unsigned int length) {
   String topicStr = String(topic);
 
-  // Serial.print("[" + topicStr + "]: ");
+  // //Serial.print("[" + topicStr + "]: ");
   // String msg = String((char*)payload);
-  // Serial.println(msg);
+  // //Serial.println(msg);
 
+  // set message timestamp
+  timerr = millis();
   if(topicStr.equals(driveTopic)) {
     drive(payload);
     return;
@@ -113,8 +112,8 @@ void messageProcessor(char* topic, byte* payload, unsigned int length) {
 }
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("ESP32 SoccerBot");
+  //Serial.begin(115200);
+  //Serial.println("ESP32 SoccerBot");
   setupWifi();
   client.setServer(brokerAdd, 1883);
   client.setCallback(messageProcessor);
@@ -130,6 +129,12 @@ void setup() {
 
 void loop() {
   // pidControl();
+  if((millis()) - timerr > 1200) {    
+    setMotorPWM(0, 0);
+    setMotorPWM(0, 2);
+    setMotorPWM(0, 1);
+    setMotorPWM(0, 3);
+   }
   mqttLoop();
 }
 
