40cc9a307f530c71ee6a84a360277f29e72f4857
manduks <iam@armando.mx>
2018-10-06 21:40:32 -0500

    Mensajes simples

--- SoccerBot.ino
+++ SoccerBot.ino
@@ -3,15 +3,9 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 
-<<<<<<< HEAD
 const char* ssid = "AXTEL XTREMO-7969";
 const char* password = "03367969";
 const char* brokerAdd = "192.168.15.5";
-=======
-const char* ssid = "Wireless-N";
-const char* password = "robotois8899";
-const char* brokerAdd = "192.168.10.102";
->>>>>>> 61fd312553d298386bafa3dc4529748a66fffffc
 const char* clientId = "SoccerBot-01";
 String driveTopic = "SoccerBots/tablet-01/soccerBot-01/drive";
 unsigned long timerr;
@@ -19,23 +13,13 @@ unsigned long timerr;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-
-
-uint8_t motorIdx = 0;
-uint8_t pwmPin = 2;
-uint8_t encAPin = 15;
-uint8_t encBPin = 4;
-uint8_t cwPin = 16;
-uint8_t ccwPin = 17;
+float tupla[3] = {0, 0, 0};
 
 void setupWifi() {
-
   WiFi.begin(ssid, password);
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
-
   randomSeed(micros());
 }
 
@@ -49,7 +33,7 @@ void reconnect() {
       // Once connected, publish an announcement...
       // client.publish("outTopic", "hello world");
       // ... and resubscribe
-      client.subscribe(driveTopic.c_str());
+      client.subscribe(driveTopic.c_str(), 1);
     } else {
       //Serial.print("failed, rc=");
       //Serial.println(client.state());
@@ -71,55 +55,46 @@ void mqttLoop() {
   client.loop();
 }
 
-void drive(byte* payload) {
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& root = jsonBuffer.parseObject(payload);
-
-  if(!root.success()) {
-    //Serial.println("[JSON Error] -> parseObject error");
-    return;
+void drive(String payload, unsigned int length) {
+  uint8_t prevIdx = 0;
+  uint8_t tuplaIdx = 0;
+  for (uint8_t i = 0; i <= length; i++) {
+    if(payload.charAt(i) == ',' || i == length) {
+      tupla[tuplaIdx] = payload.substring(prevIdx, i).toFloat();
+      tuplaIdx++;
+      prevIdx = i + 1;
+    }
   }
-
-  float x = root["x"];
-  float y = root["y"];
-  setMotorPWM(x * 1024, 0);
-  setMotorPWM( - x * 1024, 2);
-  setMotorPWM(y * 1024, 1);
-  setMotorPWM( - y * 1024, 3);
-  float rotation = root["rotation"];
-  // //Serial.println("JSON Parsed: { x: " + String(x) + ", y: " + String(y) + ", rotation: " + String(rotation) + " }");
+  setMotorPWM(tupla[0] * 1024, 0);
+  setMotorPWM( - tupla[0] * 1024, 2);
+  setMotorPWM(tupla[1] * 1024, 1);
+  setMotorPWM( - tupla[1] * 1024, 3);
 }
 
-void leds(byte* payload) {
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& root = jsonBuffer.parseObject(payload);
-
-  if(!root.success()) {
-    //Serial.println("[JSON Error] -> parseObject error");
-    return;
-  }
-  uint8_t celebrate = root["celebrate"];
-  //Serial.println("JSON Parsed: { celebrate: " + String(celebrate) + " }");
-}
+// void leds(byte* payload) {
+//   StaticJsonBuffer<200> jsonBuffer;
+//   JsonObject& root = jsonBuffer.parseObject(payload);
+//
+//   if(!root.success()) {
+//     //Serial.println("[JSON Error] -> parseObject error");
+//     return;
+//   }
+//   uint8_t celebrate = root["celebrate"];
+//   //Serial.println("JSON Parsed: { celebrate: " + String(celebrate) + " }");
+// }
 
 void messageProcessor(char* topic, byte* payload, unsigned int length) {
-  String topicStr = String(topic);
-
-  // //Serial.print("[" + topicStr + "]: ");
-  // String msg = String((char*)payload);
-  // //Serial.println(msg);
+  timerr = millis();
+  String msg = String((char*)payload);
+  // Serial.println(msg);
 
   // set message timestamp
-  timerr = millis();
-  if(topicStr.equals(driveTopic)) {
-    drive(payload);
-    return;
-  }
+  drive(msg, length);
 }
 
 void setup() {
-  //Serial.begin(115200);
-  //Serial.println("ESP32 SoccerBot");
+  // Serial.begin(115200);
+  // Serial.println("ESP32 SoccerBot");
   setupWifi();
   client.setServer(brokerAdd, 1883);
   client.setCallback(messageProcessor);
@@ -135,7 +110,7 @@ void setup() {
 
 void loop() {
   // pidControl();
-  if((millis()) - timerr > 1200) {    
+  if((millis()) - timerr > 1000) {
     setMotorPWM(0, 0);
     setMotorPWM(0, 2);
     setMotorPWM(0, 1);
@@ -143,8 +118,3 @@ void loop() {
    }
   mqttLoop();
 }
-
-
-/*
-  MQTT comunication
-*/
