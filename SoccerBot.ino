7cee8a8c44e6d827ff8b5c9de3d8f354b440fec1
manduks <iam@armando.mx>
2018-10-14 21:09:24 -0500

    MQTT comeback

--- SoccerBot.ino
+++ SoccerBot.ino
@@ -1,28 +1,27 @@
 #include "PIDs.h"
 #include <WiFi.h>
-#include <WiFiMulti.h>
-#include <WiFiClientSecure.h>
-#include <WebSocketsServer.h>
+// #include <WiFiMulti.h>
+// #include <WiFiClientSecure.h>
+// #include <WebSocketsServer.h>
+#include <PubSubClient.h>
 #include <ArduinoJson.h>
-// #include <PubSubClient.h>
 
 // #define USE_SERIAL
 
 const char* ssid = "AXTEL XTREMO-7969";
 const char* password = "03367969";
-// const char* brokerAdd = "192.168.50.51";
-// const char* clientId = "SoccerBot-XX";
-// String driveTopic = "SoccerBots/tablet-01/soccerBot-01/drive";
+const char* brokerAdd = "192.168.15.6";
+const char* clientId = "SoccerBot-01";
+String driveTopic = "SoccerBots/tablet-01/soccerBot-01/drive";
 unsigned long timerr = 0;
 
-WiFiMulti WiFiMulti;
-WebSocketsServer webSocket = WebSocketsServer(81);
+// WiFiMulti WiFiMulti;
+// WebSocketsServer webSocket = WebSocketsServer(81);
 
-// WiFiClient espClient;
+WiFiClient espClient;
+PubSubClient client(espClient);
 // WiFiServer server(80);
 // WebSocketServer webSocketServer;
-
-// PubSubClient client(espClient);
 
 // x, y, r
 float x = 0, y = 0, r = 0;
@@ -48,17 +47,54 @@ void motorsStart() {
   // motorsInit(5, 18, 19, 3);
 }
 
-void hexdump(const void *mem, uint32_t len, uint8_t cols = 16) {
-	const uint8_t* src = (const uint8_t*) mem;
-	Serial.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
-	for(uint32_t i = 0; i < len; i++) {
-		if(i % cols == 0) {
-			Serial.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
-		}
-		Serial.printf("%02X ", *src);
-		src++;
-	}
-	Serial.printf("\n");
+void setupWifi() {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    #ifdef USE_SERIAL
+    Serial.println("Connecting to WiFi..");
+    #endif
+    delay(1000);
+  }
+  #ifdef USE_SERIAL
+  Serial.println("Connected to the WiFi network");
+  Serial.println(WiFi.localIP());
+  #endif
+  randomSeed(micros());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    #ifdef USE_SERIAL
+    //Serial.print("Attempting MQTT connection...");
+    #endif
+    // Attempt to connect
+    if (client.connect(clientId)) {
+      #ifdef USE_SERIAL
+      Serial.println("connected");
+      #endif
+      // Once connected, publish an announcement...
+      // client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe(driveTopic.c_str());
+    } else {
+      #ifdef USE_SERIAL
+      Serial.print("failed, rc=");
+      Serial.println(client.state());
+      #endif
+      // //Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(1000);
+    }
+  }
+}
+
+void mqttLoop() {
+  if (!client.connected()) {
+    motorsStop();
+    reconnect();
+  }
+  client.loop();
 }
 
 void drive(byte* payload) {
@@ -79,49 +115,23 @@ void drive(byte* payload) {
   x = xTemp * thetaCos - yTemp * thetaSin;
   y = xTemp * thetaSin + yTemp * thetaCos;
 
-  setMotorPWM((-x - r * 0.40) * 1024, 0);
-  setMotorPWM((x - r * 0.40) * 1024, 2);
-  setMotorPWM((y - r * 0.40) * 1024, 1);
-  setMotorPWM((-y - r * 0.40) * 1024, 3);
+  setMotorPWM((-x - r * 0.30) * 1024, 0);
+  setMotorPWM((x - r * 0.30) * 1024, 2);
+  setMotorPWM((y - r * 0.30) * 1024, 1);
+  setMotorPWM((-y - r * 0.30) * 1024, 3);
   // #ifdef USE_SERIAL
   // Serial.println("JSON Parsed: { x: " + String(x) + ", y: " + String(y) + ", r: " + String(r) + " }");
   // #endif
 }
 
-void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
-    switch(type) {
-      case WStype_DISCONNECTED:
-        #ifdef USE_SERIAL
-        Serial.printf("[%u] Disconnected!\n", num);
-        #endif
-        break;
-      case WStype_CONNECTED:
-        {
-          IPAddress ip = webSocket.remoteIP(num);
-          #ifdef USE_SERIAL
-          Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
-          #endif
-  				// send message to client
-  				webSocket.sendTXT(num, "Welcome Dear!");
-        }
-        break;
-      case WStype_TEXT:
-        timerr = millis();
-        #ifdef USE_SERIAL
-        Serial.printf("[%u] get Text: %s\n", num, payload);
-        #endif
-        drive(payload);
-        break;
-      case WStype_BIN:
-  		case WStype_ERROR:
-  		case WStype_FRAGMENT_TEXT_START:
-  		case WStype_FRAGMENT_BIN_START:
-  		case WStype_FRAGMENT:
-  		case WStype_FRAGMENT_FIN:
-  		  break;
-    }
+void messageProcessor(char* topic, byte* payload, unsigned int length) {
+  timerr = millis();
+  #ifdef USE_SERIAL
+  String msg = String((char*)payload);
+  Serial.println(msg);
+  #endif
+  drive(payload);
 }
-
 
 void setup() {
   #ifdef USE_SERIAL
@@ -129,31 +139,32 @@ void setup() {
   Serial.println("ESP32 SoccerBot");
   Serial.setDebugOutput(true);
   #endif
-  for(uint8_t t = 4; t > 0; t--) {
-      #ifdef USE_SERIAL
-      Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
-      Serial.flush();
-      #endif
-      delay(1000);
-  }
+  // for(uint8_t t = 4; t > 0; t--) {
+  //     #ifdef USE_SERIAL
+  //     Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
+  //     Serial.flush();
+  //     #endif
+  //     delay(1000);
+  // }
+  //
+  // WiFiMulti.addAP("AXTEL XTREMO-7969", "03367969");
+  //
+  // while(WiFiMulti.run() != WL_CONNECTED) {
+  //   #ifdef USE_SERIAL
+  //   Serial.println("Connecting to WiFi..");
+  //   #endif
+  //   delay(500);
+  // }
+  // #ifdef USE_SERIAL
+  // Serial.println("Connected to the WiFi network:");
+  // Serial.println(WiFi.localIP());
+  // #endif
+  // webSocket.begin();
+  // webSocket.onEvent(webSocketEvent);
 
-  WiFiMulti.addAP("AXTEL XTREMO-7969", "03367969");
-
-  while(WiFiMulti.run() != WL_CONNECTED) {
-    #ifdef USE_SERIAL
-    Serial.println("Connecting to WiFi..");
-    #endif
-    delay(500);
-  }
-  #ifdef USE_SERIAL
-  Serial.println("Connected to the WiFi network:");
-  Serial.println(WiFi.localIP());
-  #endif
-  webSocket.begin();
-  webSocket.onEvent(webSocketEvent);
-  // setupWifi();
-  // client.setServer(brokerAdd, 1883);
-  // client.setCallback(messageProcessor);
+  setupWifi();
+  client.setServer(brokerAdd, 1883);
+  client.setCallback(messageProcessor);
 
   motorsStart();
 }
@@ -162,82 +173,23 @@ void loop() {
   if((millis()) - timerr > 1000) {
     motorsStop();
    }
-  webSocket.loop();
-  // mqttLoop();
+  // webSocket.loop();
+  mqttLoop();
 }
 
-// void setupWifi() {
-//   WiFi.begin(ssid, password);
-//   while (WiFi.status() != WL_CONNECTED) {
-//     Serial.println("Connecting to WiFi..");
-//     delay(1000);
-//   }
-//   Serial.println("Connected to the WiFi network");
-//   Serial.println(WiFi.localIP());
-//
-//   server.begin();
-//   randomSeed(micros());
-// }
 
-// void reconnect() {
-//   // Loop until we're reconnected
-//   while (!client.connected()) {
-//     // Serial.print("Attempting MQTT connection...");
-//     // Attempt to connect
-//     if (client.connect(clientId)) {
-//       // Serial.println("connected");
-//       // Once connected, publish an announcement...
-//       // client.publish("outTopic", "hello world");
-//       // ... and resubscribe
-//       client.subscribe(driveTopic.c_str());
-//     } else {
-//       // Serial.print("failed, rc=");
-//       //Serial.println(client.state());
-//       // //Serial.println(" try again in 5 seconds");
-//       // Wait 5 seconds before retrying
-//       delay(1000);
-//     }
-//   }
+// void hexdump(const void *mem, uint32_t len, uint8_t cols = 16) {
+// 	const uint8_t* src = (const uint8_t*) mem;
+// 	Serial.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
+// 	for(uint32_t i = 0; i < len; i++) {
+// 		if(i % cols == 0) {
+// 			Serial.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
+// 		}
+// 		Serial.printf("%02X ", *src);
+// 		src++;
+// 	}
+// 	Serial.printf("\n");
 // }
-
-// void mqttLoop() {
-//   if (!client.connected()) {
-//     motorsStop();
-//     reconnect();
-//   }
-//   client.loop();
-// }
-
-// void centerOffset(float theta = 45) {
-//   float x, y;
-//   // x = x * cos \theta - y * sin \theta
-//   // y = x * sin \theta + y * cos \theta
-//   x = tupla[0] * cos(theta) - tupla[1] * sin(theta);
-//   y = tupla[0] * sin(theta) + tupla[1] * cos(theta);
-//   tupla[0] = x;
-//   tupla[1] = y;
-// }
-
-// void drive(String payload, unsigned int length) {
-//   uint8_t prevIdx = 0;
-//   uint8_t tuplaIdx = 0;
-//   for (uint8_t i = 0; i <= length; i++) {
-//     if(payload.charAt(i) == ',' || i == length) {
-//       tupla[tuplaIdx] = payload.substring(prevIdx, i).toFloat();
-//       tuplaIdx++;
-//       prevIdx = i + 1;
-//     }
-//     if(tuplaIdx > 3) {
-//       break;
-//     }
-//   }
-//   centerOffset();
-//   // setMotorPWM((tupla[0] - tupla[2] * 0.35) * 1024, 0);
-//   // setMotorPWM((- tupla[0] - tupla[2] * 0.35) * 1024, 2);
-//   // setMotorPWM((tupla[1] - tupla[2] * 0.35) * 1024, 1);
-//   // setMotorPWM((- tupla[1] - tupla[2] * 0.35) * 1024, 3);
-// }
-
 
 // void leds(byte* payload) {
 //   StaticJsonBuffer<200> jsonBuffer;
@@ -250,10 +202,36 @@ void loop() {
 //   uint8_t celebrate = root["celebrate"];
 //   //Serial.println("JSON Parsed: { celebrate: " + String(celebrate) + " }");
 // }
-
-// void messageProcessor(char* topic, byte* payload, unsigned int length) {
-//   timerr = millis();
-//   // String msg = String((char*)payload);
-//   // Serial.println(msg);
-//   drive(payload);
+// void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+//     switch(type) {
+//       case WStype_DISCONNECTED:
+//         #ifdef USE_SERIAL
+//         Serial.printf("[%u] Disconnected!\n", num);
+//         #endif
+//         break;
+//       case WStype_CONNECTED:
+//         {
+//           IPAddress ip = webSocket.remoteIP(num);
+//           #ifdef USE_SERIAL
+//           Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+//           #endif
+//   				// send message to client
+//   				webSocket.sendTXT(num, "Welcome Dear!");
+//         }
+//         break;
+//       case WStype_TEXT:
+//         timerr = millis();
+//         #ifdef USE_SERIAL
+//         Serial.printf("[%u] get Text: %s\n", num, payload);
+//         #endif
+//         drive(payload);
+//         break;
+//       case WStype_BIN:
+//   		case WStype_ERROR:
+//   		case WStype_FRAGMENT_TEXT_START:
+//   		case WStype_FRAGMENT_BIN_START:
+//   		case WStype_FRAGMENT:
+//   		case WStype_FRAGMENT_FIN:
+//   		  break;
+//     }
 // }
